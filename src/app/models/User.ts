85d17b222862a3504d39c777a9a24173f3ad79b5
Zach-ma <alexma4cv@gmail.com>
2020-02-26 11:58:53 +0800

    add: user model

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,8 +1,8 @@
-export interface User {
+import { generateUUID } from '../helper/uuid';
 
-
-
-
-
-
+export class User {
+  _id:string;
+  constructor(public name:string, public email?:string){
+    this._id = generateUUID();
+  }
 }
