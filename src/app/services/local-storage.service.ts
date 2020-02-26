600766855f0de95d86ab23b230126f068e7c8383
Zach-ma <alexma4cv@gmail.com>
2020-02-26 12:03:51 +0800

    add: localStorage service

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -9,4 +9,19 @@ const ls = localStorage;
 export class LocalStorageService {
 
   constructor() { }
+
+  public set(key:string, value:any):void {
+    if(!value && value=== undefined) return;
+    const arr = JSON.stringify(value);
+    ls.setItem(key, arr);
+  }
+
+  public get<T>(key:string){
+    return JSON.parse(ls.getItem(key)) as T;
+  }
+
+  public getList<T>(key:string){
+    return ls.getItem(key) ? JSON.parse(ls.getItem(key)) as T[] : [];
+  }
+
 }
